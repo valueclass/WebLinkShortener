3d28817161d6fc08dc4434c4cbedd31efa8e5d1a
valueclass <valueclass.java@gmail.com>
2022-08-20 17:36:24 +0200

    Moved API Exceptions to separate file, reformatted API.ts

--- frontend/src/api/Api.ts
+++ frontend/src/api/Api.ts
@@ -1,55 +1,17 @@
 import { Observable, of } from "rxjs";
-import { Exception } from "../utils/Exceptions";
 import { Fetch } from "../utils/Fetch";
 import { responseToJson } from "../utils/Json";
-import { isString } from "../utils/Types";
 import { then } from "../utils/Operators";
+import { ApiException, ServerException, UnauthorizedException } from "./ApiExceptions";
 
-export class ApiException extends Exception {
-    protected _status: number;
-
-    public constructor(status: number);
-    public constructor(mesage: string, status: number);
-    public constructor(status: number, cause: Exception);
-    public constructor(message: string, status: number, cause: Exception);
-    public constructor(a: number | string, b?: number | Exception, c?: Exception) {
-        const message = isString(a) ? (a as string) : null;
-        const cause = b instanceof Exception ? b : (c || null);
-        const status = isString(a) ? (b as number) : a;
-
-        if (message) {
-            if (cause) super(message, cause);
-            else super(message);
-        } else {
-            super();
-        }
-
-        this._status = status;
-    }
-
-    get status() {
-        return this._status;
-    }
-
-    get type(): string {
-        return ApiException.name;
-    }
-}
-
-export class UnauthorizedException extends ApiException {
-    public static readonly STATUS_CODE = 401;
-
-    public constructor(message: string, cause: Exception);
-    public constructor(message: string);
-    public constructor();
-    public constructor(message?: string, cause?: Exception) {
-        if (message) {
-            if (cause) super(message, UnauthorizedException.STATUS_CODE, cause);
-            else super(message, UnauthorizedException.STATUS_CODE);
-        } else {
-            if (cause) super(UnauthorizedException.STATUS_CODE, cause);
-            else super(UnauthorizedException.STATUS_CODE);
-        }
+export function ErrorResponseToException(res: ErrorResponse): ApiException {
+    switch (res.status) {
+        case UnauthorizedException.STATUS_CODE:
+            return new UnauthorizedException(res.message);
+        case ServerException.STATUS_CODE:
+            return new ServerException(res.message);
+        default:
+            return new ApiException(res.message, res.status);
     }
 }
 
@@ -63,23 +25,12 @@ export interface User {
     username: string
 }
 
-function ErrorResponseToException(res: ErrorResponse): ApiException {
-    switch (res.status) {
-        case UnauthorizedException.STATUS_CODE:
-            return new UnauthorizedException(res.message);
-        default:
-            return new ApiException(res.message, res.status);
-    }
-}
-
 function ApiFetch(request: Request | RequestInfo, init?: RequestInit): Observable<Response> {
     const req = request instanceof Request ? request : new Request(`/api/v1/${request[0] === '/' ? request.substring(1) : request}`, init);
 
     return Fetch(req)
         .pipe(
             source => new Observable(subscriber => {
-                console.trace('ApiFetch subscriber', subscriber);
-
                 source.subscribe({
                     next: res => {
                         if (res.ok) {
@@ -95,12 +46,12 @@ function ApiFetch(request: Request | RequestInfo, init?: RequestInit): Observabl
                                         subscriber.complete();
                                     },
                                     error: err => subscriber.error(err),
-                                    complete: () => {}
+                                    complete: () => { }
                                 });
                         }
                     },
                     error: err => subscriber.error(err),
-                    complete: () => {}
+                    complete: () => { }
                 });
             })
         );
---
+++ frontend/src/api/ApiExceptions.ts
@@ -0,0 +1,89 @@
+import { Exception } from "../utils/Exceptions";
+import { isString } from "../utils/Types";
+
+export class ApiException extends Exception {
+    protected _status: number;
+
+    public constructor(status: number);
+    public constructor(mesage: string, status: number);
+    public constructor(status: number, cause: Exception);
+    public constructor(message: string, status: number, cause: Exception);
+    public constructor(a: number | string, b?: number | Exception, c?: Exception) {
+        const message = isString(a) ? (a as string) : null;
+        const cause = b instanceof Exception ? b : (c || null);
+        const status = isString(a) ? (b as number) : a;
+
+        console.log('ApiException ctor message', message);
+
+        if (message) {
+            if (cause) super(message, cause);
+            else super(message);
+        } else {
+            super();
+        }
+
+        this._status = status;
+    }
+
+    get status() {
+        return this._status;
+    }
+
+    get type(): string {
+        return ApiException.name;
+    }
+
+    getDisplayMessage() {
+        return "Unknown API Exception";
+    }
+}
+
+export class UnauthorizedException extends ApiException {
+    public static readonly STATUS_CODE = 401;
+
+    public constructor(message: string, cause: Exception);
+    public constructor(message: string);
+    public constructor();
+    public constructor(message?: string, cause?: Exception) {
+        if (message) {
+            if (cause) super(message, UnauthorizedException.STATUS_CODE, cause);
+            else super(message, UnauthorizedException.STATUS_CODE);
+        } else {
+            if (cause) super(UnauthorizedException.STATUS_CODE, cause);
+            else super(UnauthorizedException.STATUS_CODE);
+        }
+    }
+
+    get type(): string {
+        return UnauthorizedException.name
+    }
+
+    getDisplayMessage(): string {
+        return this.message;
+    }
+}
+
+export class ServerException extends ApiException {
+    public static readonly STATUS_CODE = 500;
+
+    public constructor(message: string, cause: Exception);
+    public constructor(message: string);
+    public constructor();
+    public constructor(message?: string, cause?: Exception) {
+        if (message) {
+            if (cause) super(message, ServerException.STATUS_CODE, cause);
+            else super(message, ServerException.STATUS_CODE);
+        } else {
+            if (cause) super(ServerException.STATUS_CODE, cause);
+            else super(ServerException.STATUS_CODE);
+        }
+    }
+
+    get type(): string {
+        return ServerException.name;
+    }
+
+    getDisplayMessage(): string {
+        return "Internal server exception";
+    }
+}
