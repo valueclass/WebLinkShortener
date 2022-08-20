f919cfb2377496054c9a7f225bb56431112fe806
valueclass <valueclass.java@gmail.com>
2022-08-20 19:02:22 +0200

    Added Blueprint library

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,4 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
-createRoot(document.getElementById("root")!).render(<StrictMode><App/></StrictMode>);
+import './index.css'
+
+createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
