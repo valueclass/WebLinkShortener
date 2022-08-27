3fca75e53e515834c65853016254b08c2cdbd238
valueclass <valueclass.java@gmail.com>
2022-08-27 19:35:56 +0200

    Made Shorten use LinkValidator

--- frontend/src/components/Shorten.tsx
+++ frontend/src/components/Shorten.tsx
@@ -4,35 +4,10 @@ import { useNavigate } from "react-router";
 import { Subscription } from "rxjs";
 import { ShortenLink } from "../api/Api";
 import { Exception } from "../utils/Exceptions";
+import { LinkValidator } from "../utils/Validators";
 import { useLoginGuard } from "./AppState";
 import { ExceptionCard } from "./ExceptionCard";
 import { LoadingOverlay } from "./LoadingOverlay";
-
-class ValidationException extends Exception {
-    constructor(message: string) {
-        super(message);
-    }
-
-    getDisplayMessage(): string {
-        return this.message;
-    }
-
-    get type(): string {
-        return ValidationException.name;
-    }
-}
-
-function IsDestinationUrlValid(dest: string): boolean {
-    try {
-        const url = new URL(dest);
-        console.log(url);
-        return url.protocol === "https:" || url.protocol === "http:";
-    } catch (_) {
-        return false;
-    }
-}
-
-const nameRegex = /^[a-zA-Z0-9_-]{1,128}$/;
 
 export function Shorten() {
     useLoginGuard();
@@ -53,31 +28,10 @@ export function Shorten() {
     const handler = (event: FormEvent) => {
         event.preventDefault();
 
-        if (!destination || destination === "") {
-            setException(new ValidationException("Empty destination"));
+        const ex = new LinkValidator(name, destination).validate();
+        if (ex) {
+            setException(ex);
             return;
-        }
-
-        if (!IsDestinationUrlValid(destination)) {
-            setException(new ValidationException("Destination is not a valid URL"));
-            return;
-        }
-
-        if (!IsDestinationUrlValid(destination)) {
-            setException(new ValidationException("Destination is not a valid URL"));
-            return;
-        }
-
-        if (name.length > 0) {
-            if (name.length > 128) {
-                setException(new ValidationException("Name cannot be longer than 128 characters"));
-                return;
-            }
-
-            if (!name.match(nameRegex)) {
-                setException(new ValidationException("Name is not valid"));
-                return;
-            }
         }
 
         const sub = ShortenLink(destination, name, private_)
@@ -102,10 +56,10 @@ export function Shorten() {
             <LoadingOverlay show={false}>
                 <form onSubmit={handler}>
                     <FormGroup label="Destination" labelFor="input-destination">
-                        <InputGroup id="input-destination" placeholder="https://example.com/" onChange={e => setDestination(e.currentTarget.value)} />
+                        <InputGroup id="input-destination" placeholder="https://example.com/" value={destination} onChange={e => setDestination(e.currentTarget.value.trim())} />
                     </FormGroup>
                     <FormGroup label="Name" labelFor="input-name">
-                        <InputGroup id="input-name" onChange={e => setName(e.currentTarget.value)} />
+                        <InputGroup id="input-name" value={name} onChange={e => setName(e.currentTarget.value.trim())} />
                     </FormGroup>
                     <FormGroup label="Private" inline={true} labelFor="checkbox-private">
                         <Checkbox style={{ margin: '4px 0px 0px 0px' }} id="checkbox-private" onChange={e => setPrivate(e.currentTarget.checked)} />
